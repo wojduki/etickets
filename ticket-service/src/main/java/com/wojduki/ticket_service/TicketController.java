81a8bac1e0daae529d9ba39602dcdb5696aceb1d
wojduki <wojduki@gmail.com>
2024-11-17 13:36:50 +0100

    api gateway configuration and dependency changed

--- ticket-service/src/main/java/com/wojduki/ticket_service/TicketController.java
+++ ticket-service/src/main/java/com/wojduki/ticket_service/TicketController.java
@@ -2,9 +2,11 @@ package com.wojduki.ticket_service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/ticket-service")
 public class TicketController {
     @Value("${eureka.instance.instance-id}")
     private String id;
