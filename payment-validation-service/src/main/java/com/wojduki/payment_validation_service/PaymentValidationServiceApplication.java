b7ea05f56ef5bc4ac36c94a8ee03a4c360ee31a5
wojduki <wojduki@gmail.com>
2024-11-20 10:38:54 +0100

    payment validation module added

---
+++ payment-validation-service/src/main/java/com/wojduki/payment_validation_service/PaymentValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wojduki.payment_validation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentValidationServiceApplication.class, args);
+	}
+
+}
---
+++ payment-validation-service/src/test/java/com/wojduki/payment_validation_service/PaymentValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wojduki.payment_validation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
