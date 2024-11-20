bc465828c5a19a869e88c0539c56334324d22a39
wojduki <wojduki@gmail.com>
2024-11-20 10:26:19 +0100

    added payment links

--- ticket-service/src/main/java/com/wojduki/ticket_service/service/TicketsService.java
+++ ticket-service/src/main/java/com/wojduki/ticket_service/service/TicketsService.java
@@ -18,8 +18,8 @@ public class TicketsService {
                 new Ticket("Kings of Leon", LocalDate.now(), 260, "image url", "https://buy.stripe.com/test_3cs9AN7o3b5dbXaaEE", 3000),
                 new Ticket("Norah Jones", LocalDate.now(), 350, "image url", "https://buy.stripe.com/test_8wM8wJ7o3c9h3qE145", 5000),
                 new Ticket("Coldplay", LocalDate.now(), 400, "image url", "https://buy.stripe.com/test_7sIbIVeQv4GP0es3ce", 10000),
-                new Ticket("U2", LocalDate.now(), 400, "image url", "purchase url", 30000),
-                new Ticket("Mrozu", LocalDate.now(), 180, "image url", "purchase url", 1500));
+                new Ticket("U2", LocalDate.now(), 400, "image url", "https://buy.stripe.com/test_6oE00d5fVgpx3qEeUX", 30000),
+                new Ticket("Mrozu", LocalDate.now(), 180, "image url", "https://buy.stripe.com/test_8wM28lgYD1uDgdqcMQ", 1500));
     }
 
     public List<Ticket> getTicketList() {
