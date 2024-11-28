5f756e5850ea1cdb1ccc4a740798b1f2dd489c32
wojduki <wojduki@gmail.com>
2024-11-28 10:14:41 +0100

    controllers updated

--- tickets-delivery-service/src/main/java/com/wojduki/tickets_delivery_service/controller/EmailController.java
+++ tickets-delivery-service/src/main/java/com/wojduki/tickets_delivery_service/controller/EmailController.java
@@ -4,9 +4,7 @@ import com.wojduki.tickets_delivery_service.service.MailService;
 import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/tickets-delivery-service")
@@ -17,7 +15,7 @@ public class EmailController {
     @GetMapping("/send-email")
     public void send(@RequestParam String email) throws MessagingException {
         mailService.sendEmail(email);
-        System.out.println("wysłano email");
+        System.out.println("wysłano email na:"+email);
     }
 
 
