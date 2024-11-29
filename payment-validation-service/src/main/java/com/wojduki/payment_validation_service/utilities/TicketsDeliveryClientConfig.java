ed2ec97630b305ae4e7add535095003878e1869c
wojduki <wojduki@gmail.com>
2024-11-29 10:44:05 +0100

    RestControllers, payment not ok

--- payment-validation-service/src/main/java/com/wojduki/payment_validation_service/utilities/TicketsDeliveryClientConfig.java
+++ payment-validation-service/src/main/java/com/wojduki/payment_validation_service/utilities/TicketsDeliveryClientConfig.java
@@ -1,16 +1,17 @@
 package com.wojduki.payment_validation_service.utilities;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
-
+@Configuration
 public class TicketsDeliveryClientConfig {
 
     @Bean
     public TicketsDeliveryClient createticketsDeliveryClient() {
         RestClient restClient = RestClient.builder()
-                .baseUrl("http://TICKETS-DELIVERY-SERVICE")
+                .baseUrl("http://tickets-delivery-service")
                 .build();
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
